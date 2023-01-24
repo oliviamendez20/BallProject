e2f689f7869c54961f7852def05b6891a33715be
OLIVIA MENDEZ <123483552+oliviamendez20@users.noreply.github.com>
2023-01-24 15:41:38 +0000

    made a comment

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 public class Main{
     public static void main (String[] args){
+      System.out.println("Made a comment in replit");
         JFrame myFrame = new JFrame();
         myFrame.setSize(500,500);
         BallPanel bPanel = new BallPanel();
